6549c816accada2f6f627e6611dc60fed738432e
RyanGAllen <alenryan14@gmail.com>
2013-02-28 23:26:59 -0500

    Added HTML5 Geolocation and geopy library

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,9 +1,25 @@
-function initialize() {
-	var mapOptions = {
-		center: new google.maps.LatLng(42.4, -83.15),
-		zoom: 11,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	var map = new google.maps.Map(document.getElementById("map_canvas"),
-	mapOptions);
-}
+(function(){
+	if(!navigator.geolocation){
+		alert('Your browser does not support geolocation.');
+	}else{
+		navigator.geolocation.getCurrentPosition(success, error);
+	}
+})();
+
+	function success(position){
+		var lat = position.coords.latitude,
+			lon = position.coords.longitude;
+
+		var options = {
+			center: new google.maps.LatLng( lat, lon ),
+			zoom: 12,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+
+		var map = new google.maps.Map(document.getElementById('map_canvas'), options);
+	}
+
+	function error(error){
+		alert("Sorry, an error occured:\n"+ error);
+	}
+
